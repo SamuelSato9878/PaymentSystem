04d6c6fc3079c5f26479346ca2803bc514233f11
SamuelSato <Samubits123@gmail.com>
2025-04-22 22:43:10 -0300

    version 2.0

--- sistemaDePagamento/src/sistemaDePagamento/main.java
+++ sistemaDePagamento/src/sistemaDePagamento/main.java
@@ -1,29 +1,61 @@
 package sistemaDePagamento;
 
+import java.util.Scanner;
+
 public class main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		PagamentoDinheiro compra01 = new PagamentoDinheiro(0, 0);
-		PagamentoCredito compra02 = new PagamentoCredito(0, 0);
-	
-	
-		//Pagamento em dinheiro
-		compra01.setValorPagamento(120);
-		System.out.println("Valor do Pagamento: " + compra01.getValorPagamento());
-		compra01.setValorProduto(100);
-		System.out.println("Valor do Produto: " + compra01.getValorProduto());
-		compra01.processarPagamento();
-		compra01.formaPagamento();
-		System.out.println("\n");
-		
-		
-		//Pagamento em crédito
-		compra02.setValorProduto(1000);
-		compra02.setParcelas(5);
-		compra02.processarPagamento();
-		compra02.formaPagamento();
-		
-		
-	}
+        System.out.println("Olá, bem-vindo!");
+        System.out.println("Qual é a sua forma de pagamento?");
+        System.out.println("1 - Dinheiro");
+        System.out.println("2 - Crédito (taxa de 5%)");
+        System.out.print("Escolha: ");
+        int opcao = scanner.nextInt();
 
+        switch (opcao) {
+            case 1:
+                System.out.println("\n--- Pagamento em Dinheiro ---");
+                PagamentoDinheiro compraDinheiro = new PagamentoDinheiro(0, 0);
+
+                System.out.print("Informe o valor do produto: ");
+                double valorProdutoDinheiro = scanner.nextDouble();
+                compraDinheiro.setValorProduto(valorProdutoDinheiro);
+
+                System.out.print("Informe o valor fornecido pelo cliente: ");
+                double valorPagamento = scanner.nextDouble();
+                compraDinheiro.setValorPagamento(valorPagamento);
+
+                System.out.println("Valor do Produto: R$" + compraDinheiro.getValorProduto());
+                System.out.println("Valor Pago: R$" + compraDinheiro.getValorPagamento());
+
+                compraDinheiro.processarPagamento();
+                compraDinheiro.formaPagamento();
+                break;
+
+            case 2:
+                System.out.println("\n--- Pagamento em Crédito ---");
+                PagamentoCredito compraCredito = new PagamentoCredito(0, 0);
+
+                System.out.print("Informe o valor do produto: ");
+                double valorProdutoCredito = scanner.nextDouble();
+                compraCredito.setValorProduto(valorProdutoCredito);
+
+                System.out.print("Informe a quantidade de parcelas: ");
+                int parcelas = scanner.nextInt();
+                compraCredito.setParcelas(parcelas);
+
+                System.out.println("\nValor do Produto: R$" + compraCredito.getValorProduto());
+                System.out.println("Parcelas: " + compraCredito.getParcelas());
+
+                compraCredito.processarPagamento();
+                compraCredito.formaPagamento();
+                break;
+
+            default:
+                System.out.println("Opção inválida! Tente novamente.");
+        }
+
+        scanner.close();
+    }
 }
